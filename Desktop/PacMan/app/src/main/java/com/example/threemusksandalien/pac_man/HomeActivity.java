3cee88926b69f956234c6dccbc9a5081d29052b0
geekchris <christian.cuison@outlook.com>
2018-01-06 22:13:09 -0500

    added swipe gestures and music, and adjusted pac man's size and animation speed.

--- Desktop/PacMan/app/src/main/java/com/example/threemusksandalien/pac_man/HomeActivity.java
+++ Desktop/PacMan/app/src/main/java/com/example/threemusksandalien/pac_man/HomeActivity.java
@@ -1,31 +1,40 @@
 package com.example.threemusksandalien.pac_man;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.util.Log;
-import android.view.KeyEvent;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements GestureDetector.OnGestureListener{
     private PacView pacView;
+    private MediaPlayer musicPlayer;
+    private GestureDetectorCompat gestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         pacView = new PacView(this);
         setContentView(pacView);
+        //musicPlayer =  MediaPlayer.create(getApplicationContext(), R.raw.pacman_beginning);
+        //musicPlayer.start();
+        musicPlayer = MediaPlayer.create(getApplicationContext(), R.raw.pacman_beginning);
+        try {
+            musicPlayer.start();
+            synchronized (musicPlayer){
+                musicPlayer.wait(4000);
+            }
+        }catch(InterruptedException i){
+
+        }
+        musicPlayer = MediaPlayer.create(getApplicationContext(), R.raw.siren1);
+        musicPlayer.start();
+        musicPlayer.setLooping(true);
+        this.gestureDetector = new GestureDetectorCompat(this,this);
     }
     @Override
     protected void onResume() {
@@ -35,202 +44,70 @@ public class HomeActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-        pacView.pause();
     }
-    class PacView extends SurfaceView implements Runnable {
-        private Thread pacThread;
-        private SurfaceHolder ourHolder; // Allows you to edit pixels on screen
-        private volatile boolean playing;
-        private Canvas canvas;
-        private Bitmap pacAnimation;
-        private boolean isMoving;
-        private float runSpeedperSecond = 500;
-        private float manXPos = 10, manYPos = 10;
-        private int frameWidth = 15, frameHeight = 15;
-        private int frameCount = 3;
-        private int currentFrame = 0;
-        private long fps;
-        private int dir;
-        private int frameCounter = 0;
-        private long timethisFrame;
-        private long lastFrameChangeTime = 0;
-        private int frameLengthInMilliSecond = 50;
-        private Rect frameToDraw = new Rect(0, 0, frameWidth, frameHeight);
-        private RectF whereToDraw = new RectF(manXPos, manYPos, manXPos + frameWidth, frameHeight);
-        public PacView(Context mContext)
-        {
-            super(mContext);
-            ourHolder = getHolder();
-            pacAnimation = BitmapFactory.decodeResource(getResources(), R.drawable.pacman2);
-            dir = 0;
 
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float v, float v1) {
+        switch (getSlope(e1.getX(), e1.getY(), e2.getX(), e2.getY())) {
+            case 1:
+                pacView.dir = 2;
+                return true;
+            case 2:
+                pacView.dir = 1;
+                return true;
+            case 3:
+                pacView.dir = 3;
+                return true;
+            case 4:
+                pacView.dir = 0;
+                return true;
         }
-        @Override
-        public void run()
-        {
-            while(playing){
-                long startFrameTime = System.currentTimeMillis();
-                update();
-                draw();
-                timethisFrame = System.currentTimeMillis() - startFrameTime;
-                if(timethisFrame >= 1){
-                    fps = 100000/ timethisFrame;
-                }
-            }
-        }
-        public void update() {
-            if (isMoving) {
-                manXPos = manXPos + runSpeedperSecond / fps;
-
-                if (manXPos > getWidth()) {
-                    manYPos += (int) frameHeight;
-                    manXPos = 10;
-                }
-
-                if (manYPos + frameHeight > getHeight()) {
-                    manYPos = 10;
-                }
-            }
-        }
-        public void manageCurrentFrame() {
-            long time = System.currentTimeMillis();
-
-            if (isMoving) {
-                if (time > lastFrameChangeTime + frameLengthInMilliSecond) {
-                    lastFrameChangeTime = time;
-                    currentFrame++;
-
-                    if (currentFrame >= frameCount) {
-                        currentFrame = 0;
-                    }
-                }
-            }
-
-            frameToDraw.left = currentFrame * frameWidth;
-            frameToDraw.right = frameToDraw.left + frameWidth;
-        }
-        public void draw() {
-            if (ourHolder.getSurface().isValid()) {
-                canvas = ourHolder.lockCanvas();
-                canvas.drawColor(Color.BLACK);
-                whereToDraw.set((int) manXPos, (int) manYPos, (int) manXPos
-                        + frameWidth, (int) manYPos + frameHeight);
-                manageCurrentFrame();
-                if(dir == 0){
-                    switch(frameCounter){
-                        case 0:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman1), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 1:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman0), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 2:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
-                            frameCounter=0;
-                            break;
-                    }
-                }
-                else if(dir == 1){
-                    switch(frameCounter){
-                        case 0:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman4), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 1:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman3), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 2:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
-                            frameCounter=0;
-                            break;
-                    }
-                }
-                else if(dir == 2){
-                    switch(frameCounter){
-                        case 0:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman6), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 1:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman5), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 2:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
-                            frameCounter=0;
-                            break;
-                    }
-                }
-                else if(dir == 3){
-                    switch(frameCounter){
-                        case 0:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman8), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 1:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman7), null, whereToDraw, null);
-                            frameCounter++;
-                            break;
-                        case 2:
-                            canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
-                            frameCounter=0;
-                            break;
-                    }
-                }
-                ourHolder.unlockCanvasAndPost(canvas);
-            }
-        }
-        public void pause() {
-            playing = false;
-
-            try {
-                pacThread.join();
-            } catch(InterruptedException e) {
-                Log.e("ERR", "Joining Thread");
-            }
-        }
-
-        public void resume() {
-            playing = true;
-            pacThread = new Thread(this);
-            pacThread.start();
-        }
-        @Override
-        public boolean onTouchEvent(MotionEvent event) {
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN :
-                    System.out.println("Hello");
-                    isMoving = !isMoving;
-                    break;
-            }
-
-            return true;
-        }
-        @Override
-        public boolean onKeyUp(int keyCode, KeyEvent event) {
-            System.out.println("wow");
-            switch (keyCode) {
-                case KeyEvent.KEYCODE_D:
-                    dir=0;
-                    System.out.println("hi");
-                    return true;
-                case KeyEvent.KEYCODE_A:
-                    dir=1;
-                    return true;
-                case KeyEvent.KEYCODE_W:
-                    dir=2;
-                    return true;
-                case KeyEvent.KEYCODE_S:
-                    dir=3;
-                    return true;
-                default:
-                    return super.onKeyUp(keyCode, event);
-            }
-
-
-        }
+        return false;
+    }
+    private int getSlope(float x1, float y1, float x2, float y2) {
+        Double angle = Math.toDegrees(Math.atan2(y1 - y2, x2 - x1));
+        if (angle > 45 && angle <= 135)
+            // top
+            return 1;
+        if (angle >= 135 && angle < 180 || angle < -135 && angle > -180)
+            // left
+            return 2;
+        if (angle < -45 && angle>= -135)
+            // down
+            return 3;
+        if (angle > -45 && angle <= 45)
+            // right
+            return 4;
+        return 0;
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
---
+++ Desktop/PacMan/app/src/main/java/com/example/threemusksandalien/pac_man/PacView.java
@@ -0,0 +1,222 @@
+package com.example.threemusksandalien.pac_man;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by squar on 1/6/2018.
+ */
+
+public class PacView extends SurfaceView implements Runnable {
+    private Thread pacThread;
+    private SurfaceHolder ourHolder; // Allows you to edit pixels on screen
+    private volatile boolean playing;
+    private Canvas canvas;
+    private Bitmap pacAnimation;
+    private boolean isMoving;
+    private float runSpeedperSecond = 1000;
+    private float manXPos = 10, manYPos = 10;
+    private int frameWidth = 30, frameHeight = 30;
+    private int frameCount = 3;
+    private int currentFrame = 0;
+    private long fps;
+    protected int dir;
+    private int frameCounter = 0;
+    private long timethisFrame;
+    private long lastFrameChangeTime = 0;
+    private int frameLengthInMilliSecond = 50;
+    private Rect frameToDraw = new Rect(0, 0, frameWidth, frameHeight);
+    private RectF whereToDraw = new RectF(manXPos, manYPos, manXPos + frameWidth, frameHeight);
+
+    public PacView(Context mContext) {
+        super(mContext);
+        ourHolder = getHolder();
+        pacAnimation = BitmapFactory.decodeResource(getResources(), R.drawable.pacman2);
+        dir = 0;
+        isMoving = !isMoving;
+
+    }
+
+    @Override
+    public void run() {
+        while (playing) {
+            long startFrameTime = System.currentTimeMillis();
+            update();
+            draw();
+            try{
+                Thread.sleep(30);
+            }catch(InterruptedException i){
+            }
+            timethisFrame = System.currentTimeMillis() - startFrameTime;
+            if (timethisFrame >= 1) {
+                fps = 100000 / timethisFrame;
+            }
+        }
+    }
+
+    public void update() {
+        if (isMoving) {
+            if(dir == 0) manXPos += 5;
+            else if(dir == 1) manXPos -=5;
+            else if(dir == 2) manYPos -=5;
+            else if(dir == 3) manYPos +=5;
+            if (manXPos > getWidth()) {
+
+            }
+            if (manYPos + frameHeight > getHeight()) {
+
+            }
+            else{
+
+            }
+        }
+    }
+
+    public void manageCurrentFrame() {
+        long time = System.currentTimeMillis();
+
+        if (isMoving) {
+            if (time > lastFrameChangeTime + frameLengthInMilliSecond) {
+                lastFrameChangeTime = time;
+                currentFrame++;
+
+                if (currentFrame >= frameCount) {
+                    currentFrame = 0;
+                }
+            }
+        }
+
+        frameToDraw.left = currentFrame * frameWidth;
+        frameToDraw.right = frameToDraw.left + frameWidth;
+    }
+
+    public void draw() {
+        if (ourHolder.getSurface().isValid()) {
+            canvas = ourHolder.lockCanvas();
+            canvas.drawColor(Color.BLACK);
+            whereToDraw.set((int) manXPos, (int) manYPos, (int) manXPos
+                    + frameWidth, (int) manYPos + frameHeight);
+            manageCurrentFrame();
+            if (dir == 0) {
+                switch (frameCounter) {
+                    case 0:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman1), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 1:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman0), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 2:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
+                        frameCounter = 0;
+                        break;
+                }
+            } else if (dir == 1) {
+                switch (frameCounter) {
+                    case 0:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman4), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 1:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman3), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 2:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
+                        frameCounter = 0;
+                        break;
+                }
+            } else if (dir == 2) {
+                switch (frameCounter) {
+                    case 0:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman6), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 1:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman5), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 2:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
+                        frameCounter = 0;
+                        break;
+                }
+            } else if (dir == 3) {
+                switch (frameCounter) {
+                    case 0:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman8), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 1:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman7), null, whereToDraw, null);
+                        frameCounter++;
+                        break;
+                    case 2:
+                        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pacman2), null, whereToDraw, null);
+                        frameCounter = 0;
+                        break;
+                }
+            }
+            ourHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void pause() {
+        playing = false;
+        try {
+            pacThread.join();
+        } catch (InterruptedException e) {
+            Log.e("ERR", "Joining Thread");
+        }
+    }
+
+    public void resume() {
+        playing = true;
+        pacThread = new Thread(this);
+        pacThread.start();
+    }
+
+    /*@Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                isMoving = !isMoving;
+                break;
+        }
+
+        return true;
+    }*/
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        System.out.println("wow");
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_D:
+                dir = 0;
+                System.out.println("hi");
+                return true;
+            case KeyEvent.KEYCODE_A:
+                dir = 1;
+                return true;
+            case KeyEvent.KEYCODE_W:
+                dir = 2;
+                return true;
+            case KeyEvent.KEYCODE_S:
+                dir = 3;
+                return true;
+            default:
+                return super.onKeyUp(keyCode, event);
+        }
+    }
+}
